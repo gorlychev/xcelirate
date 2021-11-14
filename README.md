51d637b15bd3636ef1902167bce8e49c82d23de1
Vitaly Gorlychev <gorlychev@gmail.com>
2021-11-14 19:45:56 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+ # Xcelirate test
+ 
+- Change database configuration in inc/config.php
+- Set 777 or 666 permissions to cache folder
+- Comment or uncomment lines in index.php according to your URL
+
+
+## Examples
+
+- https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=2
+- https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=20
+- https://xcelirate.gorlychev.com/index.php/shout/vitaly-gorlychev?limit=2
+- curl -s https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=2
+- curl -s https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=20
+- curl -s https://xcelirate.gorlychev.com/index.php/shout/vitaly-gorlychev?limit=2
--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require __DIR__ . "/inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-if ((isset($uri[3]) && $uri[3] != 'shout') || !isset($uri[4])) {
+//if ((isset($uri[2]) && $uri[2] != 'shout') || !isset($uri[3])) { // if you use url like https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=2
+if ((isset($uri[3]) && $uri[3] != 'shout') || !isset($uri[4])) { // if you use url like http://localhost/xcelirate/index.php/shout/steve-jobs?limit=1
     header("HTTP/1.1 404 Not Found");
     exit();
 }
@@ -13,6 +14,7 @@ if ((isset($uri[3]) && $uri[3] != 'shout') || !isset($uri[4])) {
 require PROJECT_ROOT_PATH . "/Controller/Api/ShoutController.php";
 
 $objFeedController = new ShoutController();
-$authorName = $uri[4];
+//$authorName = $uri[3]; // if you use url like https://xcelirate.gorlychev.com/index.php/shout/steve-jobs?limit=2
+$authorName = $uri[4]; //if you use domain url like http://localhost/xcelirate/index.php/shout/steve-jobs?limit=1
 $objFeedController->listAction($authorName);
 ?>
